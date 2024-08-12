1581bdb1da9a00e5b3f594b9a2de561b97343c25
thdihan <thdihan@outlook.com>
2024-08-12 15:07:34 +0600

    Cors updated

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,12 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(
+  cors({
+    origin: ['https://ph-fitness-shop.vercel.app', 'http://localhost:5173'],
+    credentials: true,
+  }),
+);
 
 // application routes
 app.use('/api/v1', router);
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,12 @@
-import { Server } from 'http';
 import mongoose from 'mongoose';
 import app from './app';
-
 import config from './app/config';
-
-let server: Server;
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
 
-    server = app.listen(config.port, () => {
+    app.listen(config.port, () => {
       console.log(`app is listening on port ${config.port}`);
     });
   } catch (err) {
@@ -19,18 +15,3 @@ async function main() {
 }
 
 main();
-
-process.on('unhandledRejection', (err) => {
-  console.log(`😈 unahandledRejection is detected , shutting down ...`, err);
-  if (server) {
-    server.close(() => {
-      process.exit(1);
-    });
-  }
-  process.exit(1);
-});
-
-process.on('uncaughtException', () => {
-  console.log(`😈 uncaughtException is detected , shutting down ...`);
-  process.exit(1);
-});
